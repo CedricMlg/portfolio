11f8f22e134e5e510f721c8dad588b32d03d7c95
CedricMlg <malingre.cedric@gmail.com>
2023-05-02 18:18:57 +0200

    Add projects data and images

---
+++ src/components/Work/index.jsx
@@ -0,0 +1,12 @@
+import Project from "../Project";
+
+export default function Work() {
+  return (
+    <div className="work">
+      <h6 className="work__title">
+        My <span>work</span>
+      </h6>
+      <Project />
+    </div>
+  );
+}

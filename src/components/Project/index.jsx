876ed7d7d5006834e3b6374a752c0221b4f568a3
CedricMlg <malingre.cedric@gmail.com>
2023-05-08 14:45:57 +0200

    Add style to work

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -4,5 +4,13 @@ export default function Project({ project }) {
   Project.propTypes = {
     project: PropTypes.string.isRequired,
   };
-  return <img className="project__logo" src={project.logo} alt={`Logo of ${project.name}`} />;
+  return (
+    <div className="project__block-logo">
+      <img
+        className="project__logo"
+        src={project.logo}
+        alt={`Logo of ${project.name}`}
+      />
+    </div>
+  );
 }

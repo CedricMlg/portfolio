bb6557fd6e16efcfeab42d8102ceb8a9689d27f0
CedricMlg <malingre.cedric@gmail.com>
2023-05-25 16:20:08 +0200

    Add skill page content with style

--- src/pages/Skills/index.jsx
+++ src/pages/Skills/index.jsx
@@ -1,7 +1,35 @@
+import { development, tools } from "../../data/skills.json";
+
 export default function Skills() {
   return (
     <main>
-      <div className="skillsPage">index</div>
+      <div className="skillsPage">
+        <div className="skillsPage__block-dev">
+          <h4 className="skillsPage__dev-title">Development</h4>
+          <div className="skillsPage__dev">
+            {" "}
+            {development.map((element, index) => (
+              <div key={index} className="skillsPage__element">
+                <img src={element.logo} />
+                <p>{element.name}</p>
+              </div>
+            ))}
+          </div>
+          <div className="skillsPage__chart"></div>
+        </div>
+        <div className="skillsPage__block-tools">
+          <h4 className="skillsPage__tools-title">Tools</h4>
+          <div className="skillsPage__tools">
+            {" "}
+            {tools.map((element, index) => (
+              <div key={index} className="skillsPage__element">
+                <img src={element.logo} />
+                <p>{element.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
